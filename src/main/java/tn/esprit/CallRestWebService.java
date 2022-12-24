fa476891a067bf273f6a1a4cd33664b41a086ad3
bilel sadraoui <bilel.sadraoui@esprit.tn>
2022-12-24 15:13:15 +0100

    troisieme commit

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -35,7 +35,7 @@ public class CallRestWebService {
 
 			//ip = responseObj.getString("origin");
 			ip = responseObj.getString("query");
-			System.out.println("ip : " + ip); 
+			System.out.println("ip 5 : " + ip);
 
 		} catch (IOException e) {
 			e.printStackTrace();
